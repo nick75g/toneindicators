5c8c7fba689f9a91f72ab5307491db70c0bb923f
Nick75 <nsmolkin@hotmail.com>
2024-08-27 13:23:18 +0200

    Update to new Flutter/Dart
    Flutter and Dart updated, adjustments to app

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,10 @@ import 'package:path/path.dart';
 
 var testData = {
   "/s": ["sarcasm", "The previous statement was meant to be sarcastic."],
-  "/j": ["joke", "The previous statement was meant as a joke."]
+  "/j": ["joke", "The previous statement was meant as a joke."],
+  "/lh": ["lighthearted", "The previous statement was meant to be lighthearted."],
+  "/gen": ["genuine", "The previous statement was genuine."],
+  "/srs": ["serious", "The previous statement was to be taken seriously."]
 };
 
 void main() {
@@ -23,7 +26,7 @@ class MyApp extends StatelessWidget {
         title: 'ToneIndicator',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 75, 75, 255)),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 100, 100, 255)),
         ),
         home: MyHomePage(),
       ),
@@ -85,8 +88,8 @@ class GeneratorPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final leadstyle = theme.textTheme.headlineSmall!.copyWith(color: theme.colorScheme.onPrimary);
-    final superstyle = theme.textTheme.headlineSmall!.copyWith(color: theme.colorScheme.onPrimary);
     final substyle = theme.textTheme.bodySmall!.copyWith(color: theme.colorScheme.onPrimary);
+    final titlestyle = theme.textTheme.headlineMedium!.copyWith(color: theme.colorScheme.primary);
     var appState = context.watch<MyAppState>();
     var ind = appState.indicators;
 
@@ -95,18 +98,17 @@ class GeneratorPage extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(5.0),
-            child: Text("Indicators"),
+            child: Text("Indicators", style: titlestyle),
           ),
-          SizedBox(height: 5),
           for (var i in ind.entries)
             Padding(
               padding: const EdgeInsets.all(5.0),
               child: ListTile(
                 tileColor: theme.colorScheme.primary,
-                title: Text(i.value[0], style: superstyle),
+                title: Text(i.value[0], style: leadstyle),
                 subtitle: Text(i.value[1], style: substyle),
                 leading: Text(i.key, style: leadstyle),
-                trailing: Icon(Icons.info, color: theme.colorScheme.onPrimary),
+                trailing: IconButton(onPressed: () {appState.removeIndicator(i.key);}, icon: Icon(Icons.delete, color: theme.colorScheme.onPrimary),),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15.0)
                 ),
